bd22a7c96309a8aefcf08d956d07b028ffc8e3e3
grzeznx2 <grzeznx2@gmail.com>
2022-11-28 21:52:54 +0100

    Create token mocks

--- contracts/Bat.sol
+++ contracts/Bat.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Dai is ERC20{
-  constructor() ERC20("DAI Stablecoin", "DAI"){}
+contract Bat is ERC20{
+  constructor() ERC20("Brave browser token", "BAT"){}
 }
--- contracts/Zrx.sol
+++ contracts/Zrx.sol
@@ -3,6 +3,6 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Dai is ERC20{
-  constructor() ERC20("DAI Stablecoin", "DAI"){}
+contract Zrx is ERC20{
+  constructor() ERC20("0x token", "ZRX"){}
 }
