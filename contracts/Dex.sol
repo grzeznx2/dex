24b969f6b4d47960ea50a5e003c401dea446faee
grzeznx2 <grzeznx2@gmail.com>
2022-11-28 16:53:12 +0100

    Deposit funds

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/IERC20/IERC20.sol";
 
 contract Dex {
     struct Token {
         bytes32 ticker;
         address tokenAddress;
     }
+    mapping(address => mapping(bytes32 => uint256)) public traderBalances;
     mapping(bytes32 => Token) public tokens;
     bytes32[] public tokenList;
     address public admin;
@@ -19,6 +20,11 @@ contract Dex {
     modifier onlyAdmin() {
         require(msg.sender == admin, "DEX: Only admin can perform this action");
         _;
+    }
+
+    function deposit(uint256 amount, bytes32 ticker) external {
+        IERC20(tokens[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        traderBalances[msg.sender][ticker] += amount;
     }
 
     function addToken(bytes32 ticker, address tokenAddress) external onlyAdmin {
